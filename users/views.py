b3a2b43d90c4851c4f3e2b1c48ef170e60c81145
sirgama <sirengogamaliel@gmail.com>
2022-06-11 17:58:19 +0300

    add first API view to push registered user to db

--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
 # Create your views here.
 class RegisterView(APIView):
     def post(self, request):
-        pass
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
