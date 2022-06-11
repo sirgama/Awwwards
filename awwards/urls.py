2f2b2469739ae8f202746a227efd0be5a7b50a07
sirgama <sirengogamaliel@gmail.com>
2022-06-11 17:06:59 +0300

    create register url patterns

--- awwards/urls.py
+++ awwards/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('users.urls')),
     path('', include('projects.urls')),
 ]
 if settings.DEBUG:
--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
