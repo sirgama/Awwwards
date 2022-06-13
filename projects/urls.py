1dbce9b013619edafa4516f46f80e00a0289a184
sirgama <sirengogamaliel@gmail.com>
2022-06-13 13:18:58 +0300

    create single site url pattern

--- projects/urls.py
+++ projects/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='homepage' ),
     path('sites/new/', views.NewSite, name='new-site'),
+    path('sites/<int:pk>', views.View_site, name='view-site'),
 ]
