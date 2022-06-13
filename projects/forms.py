750c6734c0ff5b44ee81c98bbbacce523aa18b24
sirgama <sirengogamaliel@gmail.com>
2022-06-13 11:39:38 +0300

    create a new site field

--- projects/forms.py
+++ projects/forms.py
@@ -9,6 +9,10 @@ class NewSiteForm(forms.ModelForm):
     url =forms.URLField(widget=forms.URLField(attrs={'class': 'Input', 'placeholder': 'Enter URL to the Website'}), required=True)
     description = forms.CharField(widget=forms.TextInput(attrs={'class': 'Input', 'placeholder': 'Project description '}), required=True)
     category =forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={'class': 'Input', 'placeholder': 'Choose a category'}),choices=CATEGORIES_1, required=True)
-    tag = forms.CharField(widget=forms.TextInput(attrs={'class': 'Input', 'placeholder': 'Image Name '}), required=True)
-    tag = forms.CharField(widget=forms.TextInput(attrs={'class': 'Input', 'placeholder': 'Image Name '}), required=True)
+    country = forms.CharField(widget=forms.TextInput(attrs={'class': 'Input', 'placeholder': 'Country '}), required=True)
+    technologies = forms.CharField(widget=forms.TextInput(attrs={'class': 'Input', 'placeholder': 'Technologies used | separate with comma '}), required=True)
     image = forms.ImageField(required=True)
+    
+    class Meta:
+        model = Site
+        fields = '__all__'
