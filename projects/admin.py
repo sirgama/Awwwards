c06a54fb8a09e65e44b1afb942bded2cef9d227e
sirgama <sirengogamaliel@gmail.com>
2022-06-13 07:12:43 +0300

    register new models to admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Technologies, Site, Rating
 # Register your models here.
+
+admin.site.register(Technologies)
+admin.site.register(Site)
+admin.site.register(Rating)
