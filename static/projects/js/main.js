72637b77b0a4621fd01efe8b05b62dcfb728e68b
sirgama <sirengogamaliel@gmail.com>
2022-06-13 14:43:04 +0300

    add javascript function to get all start elements by ID

--- static/projects/js/main.js
+++ static/projects/js/main.js
@@ -1,1 +1,20 @@
-console.log('Homes working')
+console.log('Homes ')
+
+const one = document.getElementById('first')
+const two = document.getElementById('second')
+const three = document.getElementById('third')
+const four = document.getElementById('fourth')
+const five = document.getElementById('fifth')
+const six = document.getElementById('sixth')
+const seven = document.getElementById('seventh')
+const eight = document.getElementById('eighth')
+const nine = document.getElementById('ninth')
+const ten = document.getElementById('tenth')
+
+console.log(one)
+
+const arr = [one, two, three, four, five, six, seven, eight, nine, ten]
+
+arr.forEach(item=> item.addEventListener('mouseover', (event)=>{
+    console.log(event.target)
+}))
