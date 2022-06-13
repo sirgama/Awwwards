4c14db1fbb2f7e0eed73237ebc44f6a8b0b84e3d
sirgama <sirengogamaliel@gmail.com>
2022-06-13 09:45:32 +0300

    list projects on homepage

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .models import Site, Technologies, Rating
+
+from users.models import Profile
 
 # Create your views here.
 
 def home(request):
+    user = request.user
+    site_items = Site.objects.all()
+    all_users = User.objects.all()
     
-    return render(request, 'projects/home.html')
+    context = {
+        "sites":site_items,
+        "users": all_users,
+        "current_user": user,
+    }
+    return render(request, 'projects/home.html', context)
