c0d1c0eeccd3f0eb1ab141303c637c301284d9c6
sirgama <sirengogamaliel@gmail.com>
2022-06-13 15:14:08 +0300

    add javascript function to highlight stars on mose over

--- static/projects/js/main.js
+++ static/projects/js/main.js
@@ -11,10 +11,73 @@ const eight = document.getElementById('eighth')
 const nine = document.getElementById('ninth')
 const ten = document.getElementById('tenth')
 
-console.log(one)
+const form = document.querySelector('.rate-form')
+const confirmBox = document.getElementById('confirm-box')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
 
-const arr = [one, two, three, four, five, six, seven, eight, nine, ten]
+const handleStarSelect = (size) => {
+    const children = form.children
+    for (let i=0; i< children.length; i++){
+        if(i<= size){
+            children[i].classList.add('checked')
+        } else {
+            children[i].classList.remove('checked')
+        }
+    }
+}
+
+
+console.log(one)
+const handleSelect = (selection) => {
+    switch(selection){
+        case 'first':{
+            handleStarSelect(1)
+            return
+        }
+        case 'second':{
+            handleStarSelect(2)
+            return
+        }
+        case 'third':{
+            handleStarSelect(3)
+            return
+        }
+        case 'fourth':{
+            handleStarSelect(4)
+            return
+        }
+        case 'fifth':{
+            handleStarSelect(5)
+            return
+        }
+        case 'sixth':{
+            handleStarSelect(6)
+            return
+        }
+        case 'seventh':{
+            handleStarSelect(7)
+            return
+        }
+        case 'eighth':{
+            handleStarSelect(8)
+            return
+        }
+        case 'ninth':{
+            handleStarSelect(9)
+            return
+        }
+        case 'tenth':{
+            handleStarSelect(10)
+            return
+        }
+    }
+}
+if (one){
+    const arr = [one, two, three, four, five, six, seven, eight, nine, ten]
 
 arr.forEach(item=> item.addEventListener('mouseover', (event)=>{
-    console.log(event.target)
+    handleSelect(event.target.id)
 }))
+
+}
+
